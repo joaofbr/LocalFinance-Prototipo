712e5f0506fe51da62ef20ba27e4e731e5963b1c
joaofbr <95386537+joaofbr@users.noreply.github.com>
2026-08-14 14:35:19 -0300

    fix: update FrontendBaseUrl to use production URL

--- backend/src/LocalFinance.Application/Common/InviteOptions.cs
+++ backend/src/LocalFinance.Application/Common/InviteOptions.cs
@@ -4,7 +4,7 @@ public class InviteOptions
 {
     public const string SectionName = "Invite";
 
-    public string FrontendBaseUrl { get; set; } = "http://localhost:5173";
+    public string FrontendBaseUrl { get; set; } = "https://localfinance.pages.dev";
 
     public int ExpiryHours { get; set; } = 48;
 }
